1c413b3b9d747ff556369dc5195f8041d02ba443
Zulfathi Hanafi <fathiimran7@gmail.com>
2023-11-17 22:03:44 +0800

    Draw

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import ReactDOM from "react-dom";
-import CanvasDraw from "react-canvas-draw";
+
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+
+const styles = {
+  border: '0.0625rem solid #9c9c9c',
+  borderRadius: '0.25rem',
+};
+
+const Canvas = () => {
+  return (
+    <ReactSketchCanvas
+      style={styles}
+      width="600"
+      height="400"
+      strokeWidth={4}
+      strokeColor="red"
+    />
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <CanvasDraw/>
+        <Canvas />
       </header>
     </div>
   );
